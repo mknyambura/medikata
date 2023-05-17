d519c71025445fd51114f325de314397210145ed
mknyambura <kariuki.mercie@gmail.com>
2023-05-17 11:48:29 +0300

    create when were you born page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register'
 import Page1 from './components/Questions/Page1';
+import Page2 from './components/Questions/Page2';
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path='/' element={<SignIn/>}/>
         <Route path='/register' element={<Register/>}/>
         <Route path='/page1' element={<Page1/>}/>
+        <Route path='/page2' element={<Page2/>}/>
       </Routes>
     </div>
   )
--- src/components/Questions/Page1.jsx
+++ src/components/Questions/Page1.jsx
@@ -1,74 +1,73 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom'
+import { Transition } from '@headlessui/react';
+
+import { SlArrowLeft } from "react-icons/sl"
+
+import '../../index.css'
 
 function Page1() {
 
     const [numbers, setNumbers] = useState([]);
     const [visibleItems, setVisibleItems] = useState(0);
-    const [batchSize, setBatchSize] = useState(3);
+    const [visibleIndices, setVisibleIndices] = useState([]);
     const containerRef = useRef(null);
 
     useEffect(() => {
-      // Function to load numbers
-      const loadNumbers = () => {
-        // Calculate the number of visible items based on the container height and item height
-        const containerHeight = containerRef.current.clientHeight;
-        const itemHeight = 20; // Adjust this value based on your styling
-        const visibleItemsCount = Math.ceil(containerHeight / itemHeight);
-        setVisibleItems(visibleItemsCount);
+    if (!containerRef.current) return;
 
-        // Generate the next batch of numbers based on the visible items count
-        const startIndex = numbers.length;
-        const endIndex = startIndex + visibleItemsCount;
-        const nextNumbers = Array.from({ length: visibleItemsCount }, (_, index) => startIndex + index + 1910);
-        const filteredNumbers = nextNumbers.filter((number) => number <= 2050); // Filter numbers within the desired range
-        setNumbers((prevNumbers) => [...prevNumbers, ...filteredNumbers]);
+      // Function to load numbers
+    const loadNumbers = () => {
+    //   // Calculate the number of visible items based on the container height and item height
+        const containerHeight = containerRef.current.clientHeight;
+        const itemHeight = 10; // Adjust this value based on your styling
+        const visibleItemsCount = Math.ceil(containerHeight / itemHeight);
+        setVisibleItems(visibleItemsCount); 
+
+        // Generate all numbers in a single batch
+        const allNumbers = Array.from({ length: 2051 - 1910 }, (_, index) => index + 1910);
+        setNumbers(allNumbers);
+   
     };
 
     // Load the initial batch of numbers
-    loadNumbers();
-
-    // Add event listener for scrolling
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-        // Clean up the event listener
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, [numbers.length]);
-
-    const handleScroll = () => {
-        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        const documentHeight = document.documentElement.scrollHeight;
-        const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-        // Check if the user has scrolled to the bottom
-        // Check if the user has scrolled to the bottom based on the visible items
-        if (scrollPosition + windowHeight >= documentHeight - visibleItems * 30) {
-        // Increase the batch size based on the visible items count
-            setBatchSize((prevBatchSize) => prevBatchSize + visibleItems);
-        }
-    };
-
-
+        loadNumbers();
+    }, [])
 
     return (
-      <div className='bg-[#07362E] h-screen text-white'  style={{ maxHeight: '100vh', overflowY: 'scroll' }}>
-          <h1 className='font-bold'>When Were You <span className='text-[#fca728]'>Born?</span></h1>
-          <h1>We need this information to ensure our community stays safe</h1>
-          <Link to='/page1' className='flex rounded-lg w-full'>
-              <button type="submit" className='w-1/2 bg-[#fca728] hover:bg-opacity-80 font-bold text-gray-800 p-3 mt-9 mx-auto items-center align-center rounded-lg'>Continue</button>
-          </Link>
-        <div>
-            {numbers.map((number) => (
-                <div
-                    className='text-3xl'
-                    key={number}>
-                        
-                {number}</div>
-            ))}
-            
+      <div className=' scrollbar-hide flex flex-col bg-[#07362E] ml-4 mr-4 h-screen text-white'  style={{overflowY: 'scroll' }} ref={containerRef}>
+        <div className='mr-2 ml-2 mt-6 mb-16'>
+            <Link to='/'>
+                <button className='bg-[#08ca5c] py-4 px-4 rounded-2xl'>
+                    <SlArrowLeft/>
+                </button>
+            </Link>
+            <div className='bg-gradient-to-b from-[#036857] text-[#bg-[#08ca5c]] rounded-2xl mx-auto mt-6 mb-4'>
+                <div className='bg-[#08ca5c] w-1/3 rounded-2xl text-[#08ca5c]'>.</div>
+            </div>
         </div>
+
+        <h1 className='font-bold text-6xl'>When Were You <span className='text-[#fca728] mt-6'>born?</span></h1>
+        <h1 className='text-3xl mt-4'>We need this information to ensure our community stays safe</h1>
+
+        <div className='absolute left-[30%] top-[58%] bg-gradient-to-b from-[#07362E] h-16 w-[40%]'></div>
+        
+        {containerRef.current && (
+            <div className='flex flex-col items-center mt-10 max-h-screen overflow-y-scroll whitespace-nowrap scrollbar-hide'>
+                {numbers.map((number) => (
+                    <div 
+                        key={number}
+                        className='items-center text-gray-300 text-5xl hover:bg-lime-500 rounded-lg py-1 px-10 hover:cursor-pointer'>
+                            {number}
+                    </div>
+                ))}
+            </div>
+        )}
+        <div className='absolute left-[30%] bottom-[12%] bg-gradient-to-t from-[#07362E] h-24 w-[40%]'></div>
+
+        <Link to='/page2' className='flex rounded-lg w-full'>
+          <button type="submit" className='w-1/2 bg-[#fca728] hover:bg-opacity-80 font-bold text-gray-800 p-3 mt-9 mb-6 mx-auto items-center align-center rounded-lg'>Continue</button>
+        </Link>
       </div>
     );
 };
